0a08eb709e1fc358685c80c99e9b0281b8e50577
Isaacxshaze <162961185+ISAAC3shaze@users.noreply.github.com>
2026-04-15 15:17:07 +0530

    debug redis issue

--- app/reddis_client.py
+++ app/reddis_client.py
@@ -3,4 +3,6 @@ import os
 
 redis_url = os.getenv("redis://default:KYWpYpNbBdjWfbcNDioOkwDCAgIicEaE@mainline.proxy.rlwy.net:31827")
 
+print("DEBUG REDIS_URL:", redis_url)  # 👈 ADD THIS
+
 r = redis.from_url(redis_url, decode_responses=True)
