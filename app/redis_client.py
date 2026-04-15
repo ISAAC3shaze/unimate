b658f880e94974d4bd53f20197c332366f783797
Isaacxshaze <162961185+ISAAC3shaze@users.noreply.github.com>
2026-04-15 19:18:39 +0530

    final redis changes

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,8 +1,11 @@
 import redis
 import os
 
-redis_url = os.getenv("redis://default:KYWpYpNbBdjWfbcNDioOkwDCAgIicEaE@mainline.proxy.rlwy.net:31827")
+redis_url = os.getenv("REDIS_URL")
 
-print("DEBUG REDIS_URL:", redis_url)  # 👈 ADD THIS
+if not redis_url:
+    raise Exception("❌ REDIS_URL not set in environment")
 
 r = redis.from_url(redis_url, decode_responses=True)
+
+print("✅ Redis connected successfully")
